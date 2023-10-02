eac4266425fe4fddef37e84165eb5208d983238b
Juan Leal <juan.leal.higuera@gmail.com>
2023-10-02 17:54:40 -0500

    [+]Finalizado taller basico

--- BASICO/1-10.js
+++ BASICO/1-10.js
@@ -3,11 +3,10 @@
 // const nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39]
 // escribe tu código acá
 
-const nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39]
-
+const nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39, 28, 7]
 
 for(i = 0; i <= nums.length; i++){
-    if(i > 10){
-        console.log(nums)
+    if(nums[i] > 10){
+        console.log(nums[i])
     }
 }
---
+++ BASICO/1-11.js
@@ -0,0 +1,18 @@
+// Completa el siguiente programa e imprime la cantidad de unos (1) que haya en el arreglo:
+
+// const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1 ];
+
+// El resultado debería ser el siguiente:
+// 11
+
+const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1 ];
+
+result = []
+
+for (ones of nums) {
+    if (ones == 1) {
+        result++
+    }
+}
+
+console.log(result)
